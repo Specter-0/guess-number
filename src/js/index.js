deb7aa7145a4b25950c1cb9449afb50df8f81f1b
Specter-0 <avoid-lurk-drone@duck.com>
2024-08-09 12:27:43 +0300

    docstrings + minor fix

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,19 @@
+/**
+ * Returns random number inside specified limits
+ * @param {number} min minimal number (included)
+ * @param {number} max maximal number (included)
+ * @returns random number
+ */
 function randInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+/** Basic game logic */
 class Game {
+    /** Minimal guessed number */
+    min
+    /** Maximal guessed number */
+    max
     #num
     #steps = 0
 
@@ -12,15 +23,22 @@ class Game {
         this.reset()
     }
 
+    /** How many steps were taken in game */
     get steps() {
         return this.#steps
     }
 
+    /** Reset game */
     reset() {
         this.#steps = 0
         this.#num = randInt(this.min, this.max)
     }
 
+    /** 
+     * Try guess number
+     * @param {number} n input number
+     * @returns 0 if n == num; 1 if n > num; -1 if n < num
+     */
     guess(n) {
         this.#steps++
         if (n > this.#num) return 1
@@ -30,6 +48,7 @@ class Game {
     }
 }
 
+/** Stopwatch for counting time played in game. */
 class Stopwatch {
     #startTime = null;
     #timerId = null;
@@ -40,16 +59,19 @@ class Stopwatch {
         this.#callback = callback ?? null
     }
 
-    is_stop() {
+    /** Is stopwatch stoped */
+    get is_stop() {
         return this.#timerId === null
     }
 
+    /** Start stopwatch */
     start() {
         if (this.#timerId !== null) return
         this.#startTime = new Date() - this.#diff;
         this.#timerId = setInterval(_ => this.#updateTime(), 1000);
     }
 
+    /** Stop stopwatch */
     stop() {
         if (this.#timerId === null) return
         this.#diff = new Date() - this.#startTime
@@ -57,6 +79,7 @@ class Stopwatch {
         this.#timerId = null;
     }
 
+    /** Reset stopwatch */
     reset() {
         this.stop();
         this.#startTime = null;
@@ -66,11 +89,18 @@ class Stopwatch {
 
     #updateTime() {
         const diff = new Date(new Date() - this.#startTime);
-        if (this.#callback)
+        if (this.#callback !== null)
             this.#callback(diff.getUTCHours(), diff.getUTCMinutes(), diff.getUTCSeconds());
     }
 }
 
+/**
+ * Convert raw time to string
+ * @param {number} h hours
+ * @param {number} m minutes
+ * @param {number} s seconds
+ * @returns formated string
+ */
 const fmtTime = (h, m, s) => {
     const ts = n => n.toString().padStart(2, '0')
     return `${ts(h)}:${ts(m)}:${ts(s)}`
@@ -83,8 +113,10 @@ const txt = {
     "0": "Ты угадал число"
 }
 
+/** Settings form */
 const settings = document.forms["settings"]
 
+/** Max num field */
 const max = settings["max"]
 
 /** Game form */
@@ -113,26 +145,42 @@ const game = {
         this._game.reset()
         this._stopwatch.reset()
         ste.innerText = 0
+        out.value = ""
     },
+
     _start() {
         this._reset()
         this._stopwatch.start()
+        this._isStart = true
     },
+
     _stop() {
         this._stopwatch.stop()
         this._isStart = false
     },
-    get max() { return this._game.max },
+
+    /** Minimal guessed number */
     get min() { return this._game.min },
-    setMax(v) {
+
+    set min(v) {
+        this._game.min = v
+        this._reset()
+    },
+
+    /** Maximal guessed number */
+    get max() { return this._game.max },
+
+    set max(v) {
         this._game.max = v
         this._reset()
     },
+
+    /**
+     * Try to guess number
+     * @param {number} n input number
+     */
     guess(n) {
-        if (!this._isStart) {
-            this._start()
-            this._isStart = true
-        }
+        if (!this._isStart) this._start()
 
         result = this._game.guess(n)
         if (result === 0) this._stop()
@@ -140,19 +188,7 @@ const game = {
         inp.value = ""
         out.value = txt[result]
     }
-}
-
-const act = e => {
-    e.preventDefault()
-    inp.focus()
-    game.guess(inp.value)
-}
-
-const set = e => {
-    e.preventDefault()
-    inp.focus()
-    game.setMax(max.value)
-}
+};
 
 // Preparing for work
 (
@@ -160,8 +196,16 @@ const set = e => {
         max.value = game.max
         inp.max = game.max
         inp.min = game.min
-        settings.addEventListener("submit", set)
-        guess.addEventListener("submit", act)
+        settings.addEventListener("submit", e => {
+            e.preventDefault()
+            inp.focus()
+            game.max = max.value
+        })
+        guess.addEventListener("submit", e => {
+            e.preventDefault()
+            inp.focus()
+            game.guess(inp.value)
+        })
         inp.focus()
     }
 )()
