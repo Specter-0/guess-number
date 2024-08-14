bdd9b0d6d5df35079577aa2b5306fc7108553ea5
Specter-0 <avoid-lurk-drone@duck.com>
2024-08-14 09:44:54 +0300

    error alert rework

--- js/difficulty.js
+++ js/difficulty.js
@@ -5,15 +5,42 @@ const dCfg = {
         minElId: "min",
         maxElId: "max"
     },
-    /** Displayed text */
-    txt: {
-        /** On bad `min` and `max` values in form */
-        error: "Минимальное значение должно быть меньше максимального!"
+    alert: {
+        alertElId: "alert",
+        okBtnElId: "ok",
+        hideClass: "hiden"
+    },
+}
+
+const errorAlert = {
+    _alert: document.getElementById(dCfg.alert.alertElId),
+
+    _onOk() {
+        this._alert.classList.add(dCfg.alert.hideClass)
+    },
+
+    init() {
+        const okBtn = document.getElementById(dCfg.alert.okBtnElId)
+        okBtn.addEventListener("click", _ => this._onOk())
+    },
+
+    show() {
+        this._alert.classList.remove(dCfg.alert.hideClass)
     }
 }
 
 const difficultyForm = {
     _form: document.getElementById(dCfg.form.formElId),
+    _onError: null,
+
+    set onError(fn) {
+        this._onError = fn
+    },
+
+    _onErr(e) {
+        e.preventDefault()
+        this._onError()
+    },
 
     init() {
         const min = this._form[dCfg.form.minElId]
@@ -21,10 +48,20 @@ const difficultyForm = {
 
         this._form.addEventListener("submit", e => {
             if (min.value >= max.value) {
-                e.preventDefault()
-                alert(dCfg.txt.error)
+                this._onErr(e)
             }
         })
     }
 }
-difficultyForm.init()
+
+const dController = {
+    _alert: errorAlert,
+    _form: difficultyForm,
+
+    init() {
+        this._alert.init()
+        this._form.onError = _ => this._alert.show()
+        this._form.init()
+    },
+}
+dController.init()
